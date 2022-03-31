beaf70d45d8ae3e3adc1309bd6426ee0de04fd13
Alexis Katsaprakakis <alexis.katsaprakakis@inqbarna.com>
2022-03-31 18:20:17 +0200

    make BooksError & APIError Localized

--- Books/Dependencies/APIClient/APIClientError.swift
+++ Books/Dependencies/APIClient/APIClientError.swift
@@ -7,14 +7,20 @@
 
 import Foundation
 
-enum APIClientError: Error {
+enum APIClientError: LocalizedError {
     case emptyResponse
     case decodeError
     case cannotMakeUrl
     case parsing
     case other
+}
 
-    var message: String {
+extension APIClientError {
+    var errorDescription: String? {
+        return localizedDescription
+    }
+    
+    var localizedDescription: String? {
         return NSLocalizedString("Oooops...", comment: "")
     }
 }
--- Books/WorkersStores/Books/BooksError.swift
+++ Books/WorkersStores/Books/BooksError.swift
@@ -7,11 +7,21 @@
 
 import Foundation
 
-enum BooksError: Error {
+enum BooksError: LocalizedError {
     case cannotFetch
     case cannotParse
     case cannotCreate
     case cannotPersist
     case notFound
     case other
+}
+
+extension BooksError {
+    var errorDescription: String? {
+        return localizedDescription
+    }
+    
+    var localizedDescription: String? {
+        return NSLocalizedString("Oooops...", comment: "")
+    }
 }
